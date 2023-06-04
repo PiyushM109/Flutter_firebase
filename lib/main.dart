6e85e5cdb7118a1c57ced6eef33af7a0eeeaedf8
Piyush More <piyushmore109@gmail.com>
2023-06-04 17:43:10 +0530

    Added firebasefirestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,52 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // TO FETCH DATA OF WHOLE COLLECTION
+  // QuerySnapshot snapshot = await FirebaseFirestore.instance.collection("users").get();
+  // for (var doc in snapshot.docs) {
+  //   print(doc.data().toString());
+  // }
+
+  // TO FETCH THE PERTICULAR DOCUMNET FROM FIRESTORE USING ID
+  // DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //     .collection("users")
+  //     .doc("YypGaMRosLOHN2tMLipX")
+  //     .get();
+
+  // print(snapshot.data());
+
+  // HOW TO WRITE IN FIRESTORE this method gives automatically ID to the document
+  // Map<String, dynamic> newUserData = {
+  //   "name" : "PRANS",
+  //   "email" : "prans@gmail.com"
+  // };
+  // await FirebaseFirestore.instance.collection("users").add(newUserData);
+  // print("New User Saved");
+
+  // To manually give an id to document
+  // Map<String, dynamic> newUserData = {
+  //   "name": "kriss",
+  //   "email": "kriss@gmail.com"
+  // };
+  // await FirebaseFirestore.instance
+  //     .collection("users")
+  //     .doc("yPIYUsHuF6NqJGL10k9h")
+  //     .set(newUserData);
+  // print("New User Saved");
+
+
+  //To update the data
+  await FirebaseFirestore.instance
+      .collection("users")
+      .doc("yPIYUsHuF6NqJGL10k9h")
+      .update({
+        "email" : "kriss123@gmail.com"
+      });
+
+      print("user updated");
+
+
   runApp(MyApp());
 }
 
@@ -21,7 +68,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: (FirebaseAuth.instance.currentUser != null) ? const HomeScreen() : LoginScreen(),
+      home: (FirebaseAuth.instance.currentUser != null)
+          ? const HomeScreen()
+          : LoginScreen(),
     );
   }
 }
